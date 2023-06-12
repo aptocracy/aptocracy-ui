9ff5bceea4de36008649af3ab70174006eee201c
aptocrats-dev <dev@aptocracy.com>
2023-06-12 07:20:44 +0000

    fix module address

--- src/program/utils.ts
+++ src/program/utils.ts
@@ -2,4 +2,4 @@ import { Network, Provider } from "aptos";
 
 export const provider = new Provider(Network.DEVNET);
 export const orgModuleAddress =
-  0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10;
+ "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10";
